7d7644f0ca02e4ee6d5693b8aaca195a510fa6a1
JHallberg5100 <JHallberg5100@gmail.com>
2016-10-28 09:28:40 -0500

    release 0 and turning to workflow

---
+++ ruby/vampires.rb
@@ -0,0 +1,21 @@
+#Introduction
+puts "Hello and welcome to Werewolf Incorporated patented HR program!"
+name = ""
+age = ""
+byear = ""
+garlic = ""
+health = ""
+#Name Prompt
+puts "What is your legal name?"
+name = gets.chomp
+#Year born and age Prompt
+puts "What is your age?"
+age = gets.chomp
+puts "What year were you born in?"
+byear = gets.chomp
+#Garlic Bread Prompt
+puts "Every Wednesday is Italian day in the cafeteria.  Would you like infinate garlic bread?"
+garlic = gets.chomp
+#Health insurance prompt
+puts = "We also have health insurace.  Are you intrested?"
+health = gets.chomp
