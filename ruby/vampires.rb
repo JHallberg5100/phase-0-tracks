67967fc361a43d784461b504e769ce9cbca4678e
JHallberg5100 <JHallberg5100@gmail.com>
2016-10-28 10:33:19 -0500

    allow multiple employees to be processed

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,43 +1,51 @@
-#Introduction
+#Introduction and number of employees to be processed
 puts "Hello and welcome to Werewolf Incorporated patented HR program!"
+puts "How many employees are you looking to process today?"
+employee_num = 0
+employee_num = gets.chomp
+employee_num = employee_num.to_i
 name = ""
 age = ""
 byear = ""
 garlic = ""
 health = ""
-#Name Prompt
-puts "What is your legal name?"
-name = gets.chomp
-#Year born and age Prompt
-puts "What is your age?"
-age = gets.chomp
-age = age.to_i
-puts "What year were you born in?"
-byear = gets.chomp
-byear = byear.to_i
-#Garlic Bread Prompt
-puts "Every Wednesday is Italian day in the cafeteria.  Would you like infinate garlic bread?"
-garlic = gets.chomp
-#Health insurance prompt
-puts "We also have health insurace.  Are you intrested?"
-health = gets.chomp
-#Run tests, initialize results
-results = ""
-current_year = 2016
+#begin processing for single emp, loop for howevermany employees
+until employee_num == 0
+  #Name Prompt
+  puts "What is your legal name?"
+  name = gets.chomp
+  #Year born and age Prompt
+  puts "What is your age?"
+  age = gets.chomp
+  age = age.to_i
+  puts "What year were you born in?"
+  byear = gets.chomp
+  byear = byear.to_i
+  #Garlic Bread Prompt
+  puts "Every Wednesday is Italian day in the cafeteria.  Would you like infinate garlic bread?"
+  garlic = gets.chomp
+  #Health insurance prompt
+  puts "We also have health insurace.  Are you intrested?"
+  health = gets.chomp
+  #Run tests, initialize results
+  results = ""
+  current_year = 2016
 
-#Test age, garlic, insurance
-age_test = current_year - byear
-if age_test == age && (garlic == "yes" || health == "yes")
-  results = "Probably not a vampire"
-elsif age_test != age && (garlic == "no" || health == "no")
-  results = "Probably a vampire"
-elsif age_test != age && (garlic = "no" && health == "no")
-  results = "Almost certainly a vampire"
-else
-  results = "Results inconclusive"
+  #Test age, garlic, insurance
+  age_test = current_year - byear
+  if age_test == age && (garlic == "yes" || health == "yes")
+    results = "Probably not a vampire"
+  elsif age_test != age && (garlic == "no" || health == "no")
+    results = "Probably a vampire"
+  elsif age_test != age && (garlic = "no" && health == "no")
+    results = "Almost certainly a vampire"
+  else
+    results = "Results inconclusive"
+  end
+  #Test name
+  if name == "Drake Cula" || name == "Tu Fang"
+    results = "Definitely a vampire"
+  end
+  p results
+  employee_num -= 1
 end
-#Test name
-if name == "Drake Cula" || name == "Tu Fang"
-  results = "Definitely a vampire"
-end
-p results
