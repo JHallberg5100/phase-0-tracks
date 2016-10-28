5058dd8f7b6150fa1b960722a922e271a0bcd471
JHallberg5100 <JHallberg5100@gmail.com>
2016-10-27 20:23:17 -0500

    learning to actually use git workflow

---
+++ ruby/hamsters.rb
@@ -0,0 +1,17 @@
+#hellos and prompt for name
+puts "Hello and welcome to Hamsterville USA"
+puts "What is this hamster's name"
+name = gets.chomp
+
+vol = 0
+#Prompt for vol lvl
+until vol <= 10 && vol > 0
+puts "On like around a scale from 1-10, how squeeky is it"
+vol = gets.chomp
+if vol < 0 || vol > 10
+  puts "On like around a scale from 1-10, how squeeky is it"
+#prompt for furrrrrrr
+
+#if they can be loveable
+
+#Age
