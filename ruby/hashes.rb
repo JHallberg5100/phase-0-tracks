b61bdb00e2a0ee26d6c30485fd133ffad1cfc961
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-02 09:22:27 -0500

    Init hashes

---
+++ ruby/hashes.rb
@@ -0,0 +1,21 @@
+#Begin Program and initialize hash
+puts "Welcome to designbot 5000"
+puts "Who are you adding as a client today?"
+person = gets.chomp
+hash = {
+  :name => 0,
+  :fam_size =>0 ,
+  :trendy => 00,
+  :decor => 00,
+  :married => 00,
+  :rooms => 00,
+  :income_lvl => 00,
+  :personality => 00,
+
+}
+hash[:name] = person
+#enter data for hash
+
+#finalize data entry
+
+#give oppertunity to update keys
