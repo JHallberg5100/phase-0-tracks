be8677dc5cb32c8e190b9297f7996ad0a4e81b52
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-02 09:33:28 -0500

    Input management

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,11 @@
+#create methods
+def bool(recieve)
+  if recieve.to_s == "true" || recieve.to_s == "yes"
+    return true
+  else
+    return false
+  end
+end
 #Begin Program and initialize hash
 puts "Welcome to designbot 5000"
 puts "Who are you adding as a client today?"
@@ -15,7 +23,34 @@ hash = {
 }
 hash[:name] = person
 #enter data for hash
-
+puts "How many family members exist?"
+input = gets.chomp
+input = input.to_i
+hash[:fam_size]= input
+puts "Do they consider themselves trendy?"
+input = gets.chomp
+input =bool(input)
+hash[:trendy]= input
+puts "What is their favored type of decor?"
+input = gets.chomp
+input = input.to_s
+hash[:decor]= input
+puts "Are they married?"
+input = gets.chomp
+input = bool(input)
+hash[:married]= input
+puts "How many rooms in the dwelling?"
+input = gets.chomp
+input = input.to_i
+hash[:rooms]= input
+puts "Yearly income?"
+input = gets.chomp
+input = input.to_i
+hash[:income_lvl]= input
+puts "The clients personality in one word"
+input = gets.chomp
+input = input.to_s
+hash[:personality]= input
 #finalize data entry
-
+p hash
 #give oppertunity to update keys
