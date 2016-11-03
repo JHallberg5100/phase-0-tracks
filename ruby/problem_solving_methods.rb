755c3bc87b1bcddefb47d21ee78f71c9e17cae73
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-03 17:11:34 -0500

    o done, moving to branch

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,15 +1,21 @@
 def array_search(x, y)
   count =0
+  thing = false
   x.each do |found|
     if found == y
       p count
+      thing = true
       count +=1
     else
       count +=1
     end
+
+    end
+    if thing == false
+      p nil
   end
 end
 
 tester= [2,35,26,34,5,457,235,46,732,434,734,]
 
-array_search(tester, 5)
+array_search(tester, 76)
