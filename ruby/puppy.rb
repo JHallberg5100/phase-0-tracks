41f04136b3f909a0a4ae05bb278e1962d7e49361
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-07 20:42:55 -0600

    Making a pupper

---
+++ ruby/puppy.rb
@@ -0,0 +1,10 @@
+#----------Puppy Plan ---------------
+# Size: small, medium, large
+# Fur length: short, curly, long
+# Temperment: Cuddly, Playful, Precocious
+# Name: Whatever
+# Color:Varies
+#=============Statistics==============
+#Age: ###
+#Owner: ~~~~~
+#Purebred?:
