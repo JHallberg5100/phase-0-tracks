e7ad98f2a87182f60a417fa7503346fabe1099a4
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-08 15:28:29 -0600

    Release 2

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-  #Well shoot, I suppose this is filler
+  def self.yell_angrily(words)
+  puts  words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+  puts  "Sometimes it makes me happy to shout " + words + "!!!" + " :D"
+  end
 end
+require_relative "shout"
+Shout.yell_angrily("Bosnia")
+Shout.yell_happily("GIB CLAY")
