fa9d284b54fb367051d7c3c98e978da5da724f28
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-08 09:56:17 -0600

    Initialize Santa

---
+++ ruby/santa.rb
@@ -0,0 +1,14 @@
+class Santa
+  def speak
+    puts "Ho, ho ho!  Haaaappy holidays!"
+  end
+  def eat_milk_and_cookies(cookie)
+    puts "That was a delicious #{cookie}!"
+  end
+  def initialize(gender, ethnicity,)
+    puts "initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
