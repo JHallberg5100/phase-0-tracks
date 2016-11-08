8d849c7df094c30a1740aaa569248d36d6adc597
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-08 15:48:38 -0600

    release 3

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,36 @@
+#module Shout
+#  def self.yell_angrily(words)
+#  puts  words + "!!!" + " :("
+#  end
+
+#  def self.yell_happily(words)
+#  puts  "Sometimes it makes me happy to shout " + words + "!!!" + " :D"
+#  end
+#end
+#require_relative "shout"
+#Shout.yell_angrily("Bosnia")
+#Shout.yell_happily("GIB CLAY")
+
 module Shout
-  def self.yell_angrily(words)
-  puts  words + "!!!" + " :("
+  def yell_angrily(words)
+    puts  words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
-  puts  "Sometimes it makes me happy to shout " + words + "!!!" + " :D"
+  def yell_happily(words)
+    puts  "Sometimes it makes me happy to shout " + words + "!!!" + " :D"
   end
 end
-require_relative "shout"
-Shout.yell_angrily("Bosnia")
-Shout.yell_happily("GIB CLAY")
+
+class Activist
+  include Shout
+end
+
+class Rabble_rouser
+  include Shout
+end
+
+greenpeace = Activist.new
+greenpeace.yell_angrily("Oil spills")
+
+anarchist = Rabble_rouser.new
+anarchist.yell_happily("Chaos")
