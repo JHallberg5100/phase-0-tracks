d32f90526fe56f334d045c41697d7a6ac0f19789
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-08 13:06:21 -0600

    Commit santa

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -5,10 +5,19 @@ class Santa
   def eat_milk_and_cookies(cookie)
     puts "That was a delicious #{cookie}!"
   end
-  def initialize(gender, ethnicity,)
+  def initialize(gender, ethnicity)
     puts "initializing Santa instance..."
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
     @age = 0
   end
+end
+
+santas = []
+test_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+test_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+test_genders.length.times do |i|
+    santas << Santa.new(test_genders[i], test_ethnicities[i])
+  end
+p santas
