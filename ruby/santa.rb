866b3255476b278ecbafd1ff342454ad04ede2f5
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-08 14:43:55 -0600

    Made a santa loop

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,7 @@
 class Santa
+
+  attr_reader :age
+  attr_accessor :gender, :ethnicity
   def speak
     puts "Ho, ho ho!  Haaaappy holidays!"
   end
@@ -27,35 +30,39 @@ class Santa
       end
     end
 
-      def gender=(new_gender)
-        @gender = new_gender
-      end
-
-      def age
-        return @age
-      end
-
-      def ethnicity
-        return @ethnicity
-      end
-
-      def gender
-        return @gender
-      end
-end
 end
 santas = []
-test_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-test_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-test_genders.length.times do |i|
-    santas << Santa.new(test_genders[i], test_ethnicities[i])
+test_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "maynnoaise", "two-spirit"]
+test_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "Elder Evil", "Youth Evil", "Cereal Mascot" "N/A"]
+#test_genders.length.times do |i|
+#    santas << Santa.new(test_genders[i], test_ethnicities[i])
+#end
+
+
+#santas[3].get_mad_at('Dasher')
+#santas[3].celebrate_birthday
+#santas[3].gender= "Woozle"
+#puts santas[3]
+#puts santas[3].age
+#puts santas[3].ethnicity
+#puts santas[3].gender
+
+#For Program
+#Runs program to create santas, printing out each one as it goes.
+#Has random age as well.
+santa_list = []
+santa_count = rand(500)
+while santa_count != 0
+  @output = Santa.new(test_genders[rand(9)], test_ethnicities[rand(9)])
+  determine_age = rand(140)
+  while determine_age != 0
+    @output.celebrate_birthday
+    determine_age -= 1
+  end
+  santa_count -= 1
+  santa_list << @output
+  puts @output.gender
+  puts @output.age
+  puts @output.ethnicity
 end
-
-
-santas[3].get_mad_at('Dasher')
-santas[3].celebrate_birthday
-santas[3].gender= "Woozle"
-puts santas[3]
-puts santas[3].age
-puts santas[3].ethnicity
-puts santas[3].gender
+end
