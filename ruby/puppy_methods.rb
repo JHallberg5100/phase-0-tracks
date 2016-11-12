3b5b96c486d4c523fba26bdc0d3013327431d751
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-11 18:17:20 -0600

    Putting in puppy power without successfuly using branches...ouch

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,31 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    integer.times {puts "Woof!!!!!!"}
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+  def dog_years(integer)
+    output = 0
+    output = integer * 7
+    return output
+  end
+
+  def trick
+    puts "*Sits politely*"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance............."
+  end
 end
 
+rover = Puppy.new
+rover.fetch("Rope")
+rover.speak(15)
+rover.roll_over
+rover.dog_years(1)
+rover.trick
