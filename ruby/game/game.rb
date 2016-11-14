d5ca1c626eb4d64d488cffb763387904a354a6b2
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-13 19:29:19 -0600

    Making a game reality, with a misplaced game file

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,1 +1,14 @@
-#this is some code 
+#this is some code
+#class code: Class, Game
+#The class will have four methods
+#Guess, Check, mark, create
+#Guess will take a user input
+#Check will see if that has been guessed before, and then if its in the word
+#mark will add that guess to previously guessed, and if applicable add it to the word
+#each round it will check for completion
+#Create will be the init
+#There it will take a word from the user, and put it as the word to guess
+#Then, it will create a user visible list for _
+Class Game
+  def initialize
+    
---
+++ ruby/game/game_spec.rb
@@ -0,0 +1,11 @@
+require_relative "Game"
+let(:game){Game.new}
+describe Game do
+  it "initializes game" do
+  expect(game.start("word")).to eq "word"
+end
+  it "guesses letter" do
+  expect(game.guess("o")).to eq true
+end
+  it "Properly updates"
+  expect(game.mark(true)).to eq true
