09ca4312bffc9ec9dfebcad5013bc833e4d7f364
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-14 08:51:32 -0600

    Game mostly working

--- ruby/game/game2.0.rb
+++ ruby/game/game2.0.rb
@@ -12,11 +12,12 @@
 
 class Hangman
 
-  attr_accessor :user_word, :display_word, :tot_guess, :input_word
+  attr_accessor :user_word, :display_word, :tot_guess, :input_word, :guess_num, :word_length
 
   def initialize(word)
     @guess_num = 0
     @input_word = word
+    @word_length = word.length
     @display_word = @input_word.split("")
     if word.length < 4
       @tot_guess = 3
@@ -32,10 +33,11 @@ class Hangman
   end
 
   def guess(letter)
-    spot_num = @user_word.index(letter)
+    spot_num = self.user_word.index(letter)
     if spot_num == nil
       return false
       @guess_num += 1
+      p @guess_num
     else
       @display_word[spot_num] = letter
       return true
@@ -43,22 +45,44 @@ class Hangman
 
   end
 
+#  def check
+  #  @blank_spaces = 0
+  #  @display_word.each do |letter|
+  #    if letter == "_"
+  #      @blank_spaces += 1
+  #    else
+  #    end
+    #  if @blank_spaces == 0
+    #    @game_won = true
+#        puts "Congratulations! You won"
+  #      break
+#      else
+#        @game_won = false
+#      end
+#    end
+#  end
   def check
     @blank_spaces = 0
-    @display_word.each do |letter|
-      if letter == "_"
-        @blank_spaces += 1
+    counter = 0
+    p self.word_length
+    until counter == @word_length
+      if @display_word[counter] == @user_word[counter]
+        p "Check of #{self.display_word[counter]} is correct!"
+        counter +=1
       else
-      end
-      if @blank_spaces == 0
-        @game_won = true
-      else
-        @game_won = false
+        @blank_spaces +=1
+        counter += 1
       end
     end
-  end
-
-
+      if @blank_spaces == 0
+        p "You Win"
+        return true
+      else
+        p "Keep Trying!"
+        return false
+      end
+    
+end
 end
 
 
@@ -72,3 +96,24 @@ puts "The user word is #{tester.user_word}"
 puts "The current display word is #{tester.display_word}"
 puts "Word is #{tester.input_word}"
 puts tester.tot_guess
+#Driver code
+
+puts "Welcome to the game, please enter what word you would like to play"
+input = gets.chomp
+game = Hangman.new(input)
+while game.guess_num != game.tot_guess
+  puts "The current board is #{game.display_word}"
+  puts "Please make a guess of a single letter"
+  input = gets.chomp
+  if game.guess(input) == false
+    puts "Good try, but not good enough!"
+    p "Your remaining guesses #{game.tot_guess - game.guess_num - 1}"
+  else
+    puts "Nice guess!"
+  end
+  if game.check == true
+    game.guess_num = game.tot_guess
+  else
+    game.guess_num += 1
+  end
+end
