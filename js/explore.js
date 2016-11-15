4f71db095dcd241aef6814d146f68cd00b4a5e34
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-15 15:17:16 -0600

    Beginning first JS program

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// This is a program that will reverse all letters
+// It will do this to by taking an input, and then outputting at string that is inverse
+// The input and output will be different variables
+// First the input will start at the last letter, and then slowly move backwards over each charater.
+// That charater will be put in the first part of the output
+// then after all has been added, the output will be printed.
